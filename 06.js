82d2e36f7cff07149ab667bf28a6b8e17f0fddbb
annahico <annahico@gmail.com>
2024-04-22 19:27:22 +0200

    JS asincrono 6

--- 06.js
+++ 06.js
@@ -1,15 +1,16 @@
-//Crea una función que reciba 2 números y un callback e invoque ese
-//callback tras 1 segundo con la suma de los números como argumento.
-
-function sumaCallback(num1, num2, callback) {
-    setTimeout(() => {
-        const suma = num1 + num2;
-        callback(suma);
-    }, 3000);
+const fetchUserData = (callback)=> {
+    setTimeout(()=>{
+        const user = { id:2, name: "Anna", email: "anna@gmail.com"};
+        callback(user);
+    }, 2000);
 };
 
-const callback = (result) => {
-    console.log(result);
+
+const callbackFunction = (user) =>{
+    console.log("User data received: " + user);
 };
 
-sumaCallback(5, 3, callback);
+//USO
+console.log("Inicio..."); //bloque de codigo
+fetchUserData(callbackFunction);
+console.log("Otra Parte..."); //otro bloque de codigo
